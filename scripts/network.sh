3f54f2d4b59c53cf5901d78bcd4ce545ea543d71
Aidan Harris <mail@aidanharris.io>
2018-06-07 01:03:15 +0100

    fix regex

--- scripts/network.sh
+++ scripts/network.sh
@@ -3,7 +3,7 @@
 lsblk -a
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-sed -i 's/^#\s*GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="net.ifnames=0"' \
+sed -i 's/^#\s*GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="net.ifnames=0"/' \
   /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
